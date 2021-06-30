f8318192a618f7e65a5bb378b394fdd26c7f4c52
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2021-06-30 16:11:57 +0800

    initial fork commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MyApp = () => {
+	return (<View style={{ flex:1, justifyContent:"center", alignItems:"center"}}>
+			<Text>
+			Hello World!
+			</Text>
+		</View>);
+}
+
+export default MyApp;
